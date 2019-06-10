21ee7d0cdcc11c7f0577200b66c421c3f1999f0e
Avinash Singh <avinash.dhillon@gmail.com>
2019-06-09 20:44:07 -0500

    Successfully adding stills to page

--- assets/javascript/giffy.js
+++ assets/javascript/giffy.js
@@ -1,8 +1,9 @@
 //global variables
 
+//list of buttons preloaded on the page
 const preloadedGifs = ["dog", "cat", "cow", "kookaburra", "monkey", "horse", "lion", "bear", "zebra", "kangaroo"];
-
-
+//Array of objects that holds the data 
+let gifData = new Array({});
 
 //on page-load preload the gifs and buttons
 preloadedGifs.forEach(animal => {
@@ -12,20 +13,33 @@ displayButton(animal);
 //pick a random item from the preload list to display gifs for
 let selectedItem = preloadedGifs[Math.floor(Math.random() * (preloadedGifs.length + 1))];
 console.log(selectedItem);
-getGifs(selectedItem);
+getGifs(selectedItem, 10);/////////////just get 1 gif data for now
+
 
 function displayButton(item){
      //add a button to the button-Holder div/column
      $("#button-Holder").append('<button type="button" class="btn btn-success btn-sm">' + item + '</button>');
 }
 
-function displayGifs(itemList){
-    debugger;
-    console.log(itemList);
-    gifList.forEach(listItem, indx => {
-        //first 
-        $("#gif-Section").append();
-    });
+function displayGifStills(){
+
+///////////////////////////////////////////////
+// try to order gifs by height size ascending before displaying
+/////////////////////////////////////////////////
+
+
+    //debugger;
+    for( var i = 0; i < gifData.length; i++){
+        var htmlText =
+            '<div class="card border-success" style="max-width: 18rem;">' +
+                    '<img id="image' + i + '" class="card-img-top" src="' + gifData[i].fixedHeightStill +' alt="Card image cap">' +
+                    '<p class="card-title"> Title: ' + gifData[i].title + '</p>' +
+                    '<p class="list-group-item">Rating: ' + gifData[i].rating +'</p>' +
+                    '<a href="#">OMDB</a>' +
+            '</div>';
+    
+        $("#gif-Section").append(htmlText);
+    }
  }
 
 //on form submit event
@@ -34,8 +48,6 @@ $("#searchGif").on("submit", function(event){
     displayButton($("#searchGif").find("input").val().trim());
     displayGifs($("#searchGif").find("input").val().trim());
 });
-
-
 
 //create the AJAX query and submit to Giphy's API
 function getGifs(item, numResults){
@@ -48,8 +60,30 @@ function getGifs(item, numResults){
         url: queryText,
         method: "GET"
     }).then(function (response){
-        debugger;
-        displayGifs(response.data);
+        fillGifDataArray(response.data);
     })
 }
 
+//this data fills the gifData array with object elements
+function fillGifDataArray(rawDataArray){
+    //clear the gif data object to refresh the list
+    gifData.length = 0;
+    let newObj;
+    for(var i = 0; i < rawDataArray.length; i++){
+        newObj = {
+            id: i,
+            title: rawDataArray[i].title.toUpperCase(),
+            rating: rawDataArray[i].rating.toUpperCase(),
+            trendingOn: rawDataArray[i].trending_datetime,
+            fixedWidth: rawDataArray[i].images.fixed_width.url,
+            fixedWidthStill: rawDataArray[i].images.fixed_width_still.url,
+            fixedWidthSize: parseInt(rawDataArray[i].images.fixed_width_still.width),
+            fixedHeight: rawDataArray[i].images.fixed_height.url,
+            fixedHeightStill: rawDataArray[i].images.fixed_height_still.url,
+            fixedHeightSize: parseInt(rawDataArray[i].images.fixed_height_still.height)
+        };
+        gifData.push(newObj);
+    }
+    //display the GIF stills for all the gifs in the gifDataArray
+    displayGifStills();
+}
